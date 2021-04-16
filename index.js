97186216bb2eb48dd9707492a77bd5d594d547bc
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-04-16 11:31:55 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+const url = "http://localhost:3000/users"
+
+let formInfo ={
+    name: 'Selam',
+    email: 'selam.tadd.degefu@gmail.com'
+}
+
+function submitData(){
+let postRequest ={
+    method: "POST",
+    headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formInfo)
+}
+return fetch(url, postRequest)
+.then(res => res.json())
+.then(data => {
+    const div = document.createElement('div')
+    div.innerText = data.id
+    document.body.append(div)
+.catch(error => {
+    alert('error has occured')
+})
+})
+}
+
+submitData()
+
